4a654bd869045c4b833981f614cbaff91fdb01d9
MaryaKaysina <61281550+MaryaKaysina@users.noreply.github.com>
2020-08-22 16:58:00 +0500

    create game

--- main.js
+++ main.js
@@ -1,33 +1,82 @@
-const phone = prompt();
-
-function formattedPhone(phone) {
-    let result = '';
-
-    if (phone.length === 12) {
-        for (let i = 0; i <= 11; i++) {
-            if (i === 2) {
-                result = result + ' ' + '(' + phone.charAt(i);
-                continue;
-            }
-            if (i === 5) {
-                result = result + ') ' + phone.charAt(i);
-                continue;
-            }
-            if (i === 8) {
-                result = result + '-' + phone.charAt(i);
-                continue;
-            }
-            if (i === 10) {
-                result = result + '-' + phone.charAt(i);
-                continue;
-            }
-            result += phone.charAt(i);
-        }
-    } else {
-        result = 'Какой-то странный номер телефона!'
-    }
-    
-    return result;
+const $btnHero = document.getElementById('btn-kick-hero');
+const $btnEnemy = document.getElementById('btn-kick-enemy');
+const $btnRestart = document.getElementById('btn-restart');
+const character = {
+    name: 'Pikachu',
+    defautHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
 }
 
-console.log(formattedPhone(phone));
+const enemy = {
+    name: 'Charmander',
+    defautHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
+
+$btnHero.addEventListener('click', function () { 
+    changeHP(random(20), enemy);
+    $btnHero.disabled = true;
+    $btnEnemy.disabled = false;
+});
+
+$btnEnemy.addEventListener('click', function () { 
+    changeHP(random(20), character);
+    $btnHero.disabled = false;
+    $btnEnemy.disabled = true;
+});
+
+$btnRestart.addEventListener('click', function () { 
+    restart(character);
+    restart(enemy);
+    $btnHero.disabled = false;
+    $btnEnemy.disabled = false;
+    $btnRestart.style.display = 'none';
+});
+
+function init () { 
+    renderHP(character);
+    renderHP(enemy);
+}
+
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPLife(person) { 
+    person.elHP.innerText = person.damageHP + ' / ' + person.defautHP;
+}
+
+function renderProgressbarHP (person) {
+    person.elProgressbar.style.width = person.damageHP + '%'
+}
+
+function changeHP(count, person) {
+    if (person.damageHP < 15) {
+        person.damageHP = 0;
+        alert('Бедный ' + person.name + ' проиграл бой!');
+        $btnHero.disabled = true;
+        $btnEnemy.disabled = true;
+        $btnRestart.style.display = 'block';
+    } else {
+        person.damageHP -= count;
+    }
+
+    renderHP(person);
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
+
+function restart (person) {
+    person.damageHP = person.defautHP;
+    renderHP(character);
+    renderHP(enemy);
+}
+
+init();
